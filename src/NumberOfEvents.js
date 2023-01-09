529c963186d37a0e8e0b31b416cd6e142787eeb9
HZeligman <103866815+HZeligman@users.noreply.github.com>
2023-01-09 18:11:56 -0500

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,44 +1,34 @@
 import React, { Component } from 'react';
-import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
-    numberOfEvents: 32,
-    errorText: '',
-    events: []
-  };
-
-  changeNum = (value) => {
-    this.setState({ num: value });
-    this.props.updateNumberOfEvents(value);
-    if (value < 1 || value > 32) {
-      this.setState({ errorText: 'Please select a value between 1 and 32.' });
-    } else this.setState({ errorText: '' });
+    noe: 32
   };
 
   componentDidMount() {
-    this.setState({ num: this.props.num || 32 });
+    this.setState({ noe: this.props.noe || 32 });
+  }
+
+  changeNOE(value) {
+    this.setState({ noe: value })
   }
 
   render() {
-    const { num } = this.state;
-
+    const { noe } = this.state;
     return (
-      <div>
-        <ErrorAlert text={this.state.errorText} />
-        <label>
-          Number of Events
-          <input
-            className='num'
-            type='number'
-            value={num}
-            onChange={(event) => {
-              this.changeNum(event.target.value);
-            }}>
-          </input>
-        </label>
+      <div className='NumberOfEvents'>
+        <h3>Number of Events:</h3>
+        <input
+          className='noe-input'
+          type='number'
+          value={noe}
+          onChange={event => {
+            this.changeNOE(event.target.value);
+          }}
+        >
+        </input>
       </div>
-    );
+    )
   }
 }
 
