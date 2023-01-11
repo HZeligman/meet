aa743ebfe87663ad6eaf15248ec26e6b014097e8
HZeligman <103866815+HZeligman@users.noreply.github.com>
2023-01-10 20:31:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import { extractLocations, getEvents } from './api';
 import './nprogress.css';
+import NumberOfEvents from './NumberOfEvents';
+
 
 class App extends Component {
   state = {
     events: [],
     locations: []
+    selectedLocation: 'all',
+    eventCount: 32
   }
 
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
+        events = events.slice(0, this.state.eventCount);
         this.setState({ events, locations: extractLocations(events) });
       };
     });
@@ -24,15 +29,31 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
+  updateEvents = (location, inputNumber) => {
+    const { eventCount, selectedLocation } = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') ?
+          events :
+          events.filter((event) => event.location === location);
+        const eventsToShow = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: eventsToShow,
+          selectedLocation: location
+        });
       });
-    });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents = (selectedLocation === 'all') ?
+          events :
+          events.filter((event) => event.location === selectedLocation);
+        const eventsToShow = locationEvents.slice(0, inputNumber);
+        this.setState({
+          events: eventsToShow,
+          eventCount: inputNumber
+        });
+      })
+    }
   }
 
   render() {
