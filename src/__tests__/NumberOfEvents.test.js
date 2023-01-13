a4db7630a9e089a6082ade185aaf113e7e748518
HZeligman <103866815+HZeligman@users.noreply.github.com>
2023-01-13 17:14:02 -0500

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -2,30 +2,28 @@ import React, { Component } from 'react';
 import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
-describe('<NumberOfEvents />', () => {
-
-  let NumberOfEventsWrapper, noeInput;
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
   beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
-    noeInput = NumberOfEventsWrapper.find('input.noe-input');
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => { }} />);
   });
 
-  test('<NumberOfEvents /> and noe-input are both rendered', () => {
-    expect(NumberOfEventsWrapper).toBeDefined();
-    expect(noeInput).toBeDefined();
+  test('render textbox element', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
   });
 
-  test('noe-input is 10 (number type) by default', () => {
-    expect(NumberOfEventsWrapper.find('input.noe-input').prop('type')).toBe('number');
-    expect(NumberOfEventsWrapper.state('noe')).toBe(10);
-  })
+  test('render text input correctly', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('#numberOfEvents__input').prop('value')).toBe(numberOfEvents);
+  });
 
-  test('noe-input is changed and the value is reflected correctly', () => {
-    expect(NumberOfEventsWrapper.state('noe')).toBe(10);
-    NumberOfEventsWrapper.find('input.noe-input')
-      .simulate('change', {
-        target: { value: 15 }
-      });
-    expect(NumberOfEventsWrapper.state('noe')).toBe(15);
-  })
-})
+  test('change state when input changes', () => {
+    const eventObject = { target: { value: 32 } };
+    NumberOfEventsWrapper.find('#numberOfEvents__input').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+  });
+
+  test('show number of events input label', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents label')).toHaveLength(1);
+  });
+});
