f0f0adfd3c46a8133d82551972a740e333aea8ab
HZeligman <103866815+HZeligman@users.noreply.github.com>
2023-01-13 16:30:38 -0500

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,35 +1,44 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-
-  state = { eventCount: 32 }
-
-  handleInputChanged = (e) => {
-    const inputValue = e.target.value;
-    this.props.updateEvents(null, inputValue);
-    this.setState({
-      eventCount: inputValue,
-    })
+  state = {
+    numberOfEvents: 32,
   }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.props.updateEvents(null, value);
+    this.setState({
+      numberOfEvents: value,
+    });
+
+    if (value < 1) {
+      this.setState({
+        infoText: 'Select a number from 1 to 32.',
+      });
+    } else {
+      this.setState({
+        infoText: '',
+      });
+    }
+  };
 
   render() {
-    const { noe } = this.state;
+    const { numberOfEvents } = this.state;
     return (
       <div className="NumberOfEvents">
-        <h3># of Events:</h3>
+        <label>Number of Events:</label>
         <input
-          className="noe-input"
-          type="number"
-          value={noe}
-          onChange={event => {
-            this.handleInputChanged(event);
-          }}
-        >
-        </input>
+          type="text"
+          id="numberOfEvents__input"
+          value={numberOfEvents}
+          onChange={this.handleInputChanged}
+        />
+        <ErrorAlert text={this.state.infoText} />
       </div>
-    )
+    );
   }
-
 }
 
 export default NumberOfEvents;
