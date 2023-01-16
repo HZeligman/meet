b4c58076b617b4f76934052aa3a6d7ff26a5755c
HZeligman <103866815+HZeligman@users.noreply.github.com>
2023-01-15 19:23:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,65 +2,97 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import { extractLocations, getEvents } from './api';
-import './nprogress.css';
 import NumberOfEvents from './NumberOfEvents';
+import { getEvents, checkToken, getToken } from './api';
+import './nprogress.css';
+
 
 
 class App extends Component {
   state = {
     events: [],
+    page: null,
     locations: [],
-    selectedLocation: 'all',
-    eventCount: 32
+    currentLocation: 'all',
+    numberOfEvents: 32,
+    tokenCheck: false,
+    offlineText: '',
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    const accessToken = localStorage.getItem("access_token");
+    const validToken = accessToken !== null ? await checkToken(accessToken) : false;
+    this.setState({ tokenCheck: validToken });
+    if (validToken === true) this.updateEvents()
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        events = events.slice(0, this.state.eventCount);
-        this.setState({ events, locations: extractLocations(events) });
-      };
-    });
+    if (code && this.mounted === true && validToken === false) {
+      this.setState({ tokenCheck: true });
+      this.updateEvents()
+    }
   }
 
   componentWillUnmount() {
     this.mounted = false;
   }
 
-  updateEvents = (location, inputNumber) => {
-    const { eventCount, selectedLocation } = this.state;
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length;
+      const city = location.split(" ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
+  updateEvents = (location, eventCount) => {
+    console.log('update events token valid: ', this.state.tokenCheck)
+    const { currentLocation, numberOfEvents } = this.state;
     if (location) {
-      getEvents().then((events) => {
-        const locationEvents = (location === 'all') ?
-          events :
-          events.filter((event) => event.location === location);
-        const eventsToShow = locationEvents.slice(0, eventCount);
-        this.setState({
-          events: eventsToShow,
-          selectedLocation: location
+      getEvents().then((response) => {
+        const locationEvents =
+          location === "all"
+            ? response.events
+            : response.events.filter((event) => event.location === location);
+        const events = locationEvents.slice(0, numberOfEvents);
+        return this.setState({
+          events: events,
+          currentLocation: location,
+          locations: response.locations,
         });
       });
     } else {
-      getEvents().then((events) => {
-        const locationEvents = (selectedLocation === 'all') ?
-          events :
-          events.filter((event) => event.location === selectedLocation);
-        const eventsToShow = locationEvents.slice(0, inputNumber);
-        this.setState({
-          events: eventsToShow,
-          eventCount: inputNumber
+      getEvents().then((response) => {
+        const locationEvents =
+          currentLocation === "all"
+            ? response.events
+            : response.events.filter(
+              (evemt) => event.location === currentLocation
+            );
+        const events = locationEvents.slice(0, eventCount);
+        return this.setState({
+          events: events,
+          numberOfEvents: eventCount,
+          locations: response.locations,
         });
-      })
+      });
     }
-  }
+  };
 
   render() {
-    return (
+    const { locations, numberOfEvents, events, tokenCheck } = this.state;
+    return tokenCheck === false ? (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city.</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
+          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
+        />
       </div>
     );
   }
