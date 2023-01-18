467550efeae7ba8cf8c3ef1c03d3468458ff1193
HZeligman <103866815+HZeligman@users.noreply.github.com>
2023-01-17 22:51:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 
